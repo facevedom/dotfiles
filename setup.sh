7605e2d8b68aad25854d0881e4056800d828e93d
Felipe Acevedo <felipe.a.montoya@gmail.com>
2025-04-24 21:13:08 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 cp .zshrc ~/.zshrc
+cp .p10k.zsh ~/.p10k.zsh
 
 # setup tmux
 sudo apt install tmux -y
