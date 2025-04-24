fa77c3112fc4f8ec1f2459aebaf899a9f90fe92c
Felipe Acevedo <felipe.a.montoya@gmail.com>
2025-04-24 20:51:49 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
+# setup zsh
 sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+cp .zshrc ~/.zshrc
+
+# setup tmux
+sudo apt install tmux
+cp .tmux.conf ~/.tmux.conf
